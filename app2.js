9e91290bd3e565d7454ecca7a51624f800f0daa4
apip <apip@gitlab.lab>
2021-07-06 10:14:11 +0700

    CRUD dengan menggunakan command line

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
-const { simpanContact } = require('./contacts2');
+const { deleteContact, listContact, simpanContact, detailContact } = require('./contacts2');
 
+// menambah data
 yargs.command({
     command: 'add',
     describe: 'menambahkan contact baru',
@@ -24,6 +25,47 @@ yargs.command({
     handler(argv) {
         simpanContact(argv.nama, argv.email, argv.noHp);
     }
+}).demandCommand();
+
+// menampilkan data
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua nama contact',
+    handler() {
+        listContact();
+    }
 });
+
+// menampilkan detail
+yargs.command({
+    command: 'detail',
+    describe: 'Menampilkan detail contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        detailContact(argv.nama);
+    }
+}).demandCommand();
+
+// menghapus data
+yargs.command({
+    command: 'delete',
+    describe: 'Menghapus data contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        deleteContact(argv.nama);
+    }
+}).demandCommand();
 
 yargs.parse();
--- contacts2.js
+++ contacts2.js
@@ -14,10 +14,60 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const simpanContact = (nama, email, noHp) => {
-    const contact = { nama, email, noHp };
+const listContact = () => {
+    const contacts = loadContact();
+    console.log(chalk.cyan.inverse.bold('Daftar Kontak : '));
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama}`)
+    });
+}
+
+const detailContact = (nama) => {
+    const contacts = loadContact();
+
+    if (nama) {
+        const contact = contacts.find(c => c.nama.toLowerCase() === nama.toLowerCase());
+        if (!contact) {
+            console.log(chalk.red.inverse.bold('Nama tidak ada di Kontak!'));
+            return;
+        }
+
+        console.log(chalk.cyan.inverse.bold('Detail Kontak : '));
+        console.log(`Nama : ${contact.nama}`);
+        console.log(`Email : ${contact.email}`);
+        console.log(`No. Hp : ${contact.noHp}`);
+    } else {
+        console.log(chalk.red.inverse.bold('Nama belum diisi!'));
+    }
+}
+
+const deleteContact = (nama) => {
+    const contacts = loadContact();
+
+    if (nama) {
+        const contact = contacts.find(c => c.nama.toLowerCase() === nama.toLowerCase());
+        if (!contact) {
+            console.log(chalk.red.inverse.bold('Nama tidak ada di Kontak!'));
+            return;
+        }
+
+        const newContacts = contacts.filter(e => e.nama.toLowerCase() !== nama.toLowerCase());
+        fs.writeFileSync('data/contacts.json', JSON.stringify(newContacts));
+        console.log(chalk.green.inverse.bold(`Data Kontak dengan nama ${nama} berhasil dihapus!`));
+    } else {
+        console.log(chalk.red.inverse.bold('Nama belum diisi!'));
+    }
+}
+
+const loadContact = () => {
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(fileBuffer);
+    return contacts;
+}
+
+const simpanContact = (nama, email, noHp) => {
+    const contact = { nama, email, noHp };
+    const contacts = loadContact();
 
     // cek duplikat
     if (nama) {
@@ -51,5 +101,8 @@ const simpanContact = (nama, email, noHp) => {
 }
 
 module.exports = {
-    simpanContact
+    deleteContact,
+    simpanContact,
+    listContact,
+    detailContact
 }
